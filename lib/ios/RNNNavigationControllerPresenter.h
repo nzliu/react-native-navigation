49aa059fa378781a1d9d49db62f7e3d2ab54047b
Nathan Liu <kirbathan@gmail.com>
2019-01-17 10:42:38 -0500

    :bug: Fix ios pop gesture (#1)

--- lib/ios/RNNNavigationControllerPresenter.h
+++ lib/ios/RNNNavigationControllerPresenter.h
@@ -1,10 +1,12 @@
 #import "RNNBasePresenter.h"
 #import "RNNRootViewCreator.h"
 #import "RNNReactComponentRegistry.h"
+#import "InteractivePopGestureDelegate.h"
 
 @interface RNNNavigationControllerPresenter : RNNBasePresenter
 
 - (instancetype)initWithcomponentRegistry:(RNNReactComponentRegistry *)componentRegistry;
+@property (nonatomic, strong) InteractivePopGestureDelegate *interactivePopGestureDelegate;
 
 - (void)applyOptionsBeforePopping:(RNNNavigationOptions *)options;
 
